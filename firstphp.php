688424e0f776a0c62ede5284c60b9940acdee735
mizcaroll <carolomukule@gmail.com>
2020-09-03 16:17:13 +0300

    First commit

---
+++ firstphp.php
@@ -0,0 +1,60 @@
+<!-- comaprison operators. == equal to, <> or != not equal to. <?php
+
+$num = 10;
+$num1 = 41;
+if ($num == $num1) {
+echo "The condition is true";
+}
+else {
+  echo "The condition is idk";
+}
+?> -->
+
+<!-- elseif statement. Allows us to add multiple outcomes <?php
+
+$num = 6;
+
+if ($num>6) {
+echo "the condition is true";
+}
+elseif ($num>=6) {
+echo "the elseif was executed";
+}
+else {
+  echo "the condition is false";
+}
+?> -->
+
+<!-- if else statement <?php
+$num = 6;
+
+if ($num>6) {
+echo "the condition is true";
+}
+else {
+  echo "the condition is false";
+}
+?> -->
+
+<!-- operator precedence. BODMAS applies <?php
+
+$num = (10+5) * 4;
+echo $num;
+
+?> -->
+
+<!-- arithmetic and modular operators <?php
+
+$a=14;
+$a++;
+echo $a;
+
+?> -->
+
+<!-- string concatenation <?php
+
+$string = "indeed";
+$string .= ".com";
+echo $string
+
+?> -->
