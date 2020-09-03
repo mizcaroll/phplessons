db1952a6df0c30c95c12615bcad1134f34af46e0
mizcaroll <carolomukule@gmail.com>
2020-09-03 17:48:57 +0300

    Hello world

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>phplessons</title>
+</head>
+<body>
+  <?php
+echo "Hello world";
+  ?>
+</body>
+</html>
+
+
+
 <!-- comaprison operators. == equal to, <> or != not equal to. <?php
 
 $num = 10;
